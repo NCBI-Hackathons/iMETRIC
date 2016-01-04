d4dffc954d6c14d276f92aecef1685e5493076d9
Russell Durrett <russdurrett@gmail.com>
2016-01-04 11:58:15 -0500

    formatted for hosting

---
+++ app.py
@@ -0,0 +1,21 @@
+import sys, os 
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+# Get appropriate port from Heroku (or where ever we host)
+try: 
+ port = int(os.environ['PORT'])
+except: 
+ port = 5001 
+
+print 'running on port ' + str(port) 
+
+@app.route('/')
+def hello():
+    return 'Hello Team!'
+
+if __name__ == '__main__':
+    app.run(port=port, debug=True)
