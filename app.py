00383419437638a018143f7c4dc6597cb028af02
Russell Durrett <russdurrett@gmail.com>
2016-01-04 17:29:36 -0500

    requirements augmented

--- app.py
+++ app.py
@@ -1,7 +1,48 @@
+#System Imports 
 import sys, os
-from flask import Flask
+import json
+import static
+import time
+import random
+from shutil import copyfile
+import operator
+import urllib2
+import itertools
+import subprocess
+import math 
+# from filechunkio import FileChunkIO 
+from celery import Celery
+from collections import defaultdict, OrderedDict
+import collections
+
+#Flask Imports
+from werkzeug import secure_filename
+from flask import Flask, Blueprint, make_response, render_template, render_template_string, request, session, flash, redirect, url_for, jsonify, get_flashed_messages, send_from_directory
+from flask.ext.bcrypt import Bcrypt
+from flask.ext.login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
+from flask.ext.mail import Mail, Message
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask_bootstrap import Bootstrap
+from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
+from flask_nav import Nav 
+from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
+from flask_sqlalchemy import SQLAlchemy
+from markupsafe import escape
+import wtforms
+from flask_wtf import Form
+import random
+import jinja2 
 from flask_restful import reqparse, abort, Api, Resource
-from flask import render_template
+
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, Boolean
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.dialects.postgresql import JSON, JSONB, ARRAY, BIT, VARCHAR, INTEGER, FLOAT, NUMERIC, OID, REAL, TEXT, TIME, TIMESTAMP, TSRANGE, UUID, NUMRANGE, DATERANGE
+from sqlalchemy.sql import select
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,8 +58,6 @@ print 'running on port ' + str(port)
 @app.route('/')
 def index():
     return render_template("index.html")
-
-
 
 
 
