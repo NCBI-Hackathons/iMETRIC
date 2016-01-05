3eff7ad7a0759b34908a52e17b8f0dfd5cc71123
John Didion <github@didion.net>
2016-01-05 16:34:47 -0500

    bugfixes

--- mhcpredict/mhcpredict/tools/mp_NetMHCpan.py
+++ mhcpredict/mhcpredict/tools/mp_NetMHCpan.py
@@ -1,11 +1,13 @@
 # TODO: refactor to move code that is common between
 # this and mp_NetMHCIIPan.py to a shared base class
 
-from mhcpredict.predict import MHCPeptidePredictor
-from mhcpredict.util import create_temp_fasta, sort_by_length, rbind
+import csv
 import os
 import re
 import subprocess
+
+from mhcpredict.predict import MHCPeptidePredictor
+from mhcpredict.util import create_temp_fasta, sort_by_length, rbind
 
 def get_instance(config):
     #if IEDB locally installed
@@ -35,18 +37,17 @@ class LocalNetMHCPanPredictor(MHCPeptidePredictor):
         if len(sequences) == 0 or len(alleles) == 0:
             # TODO: warn
             return None
-        rows_list = self._predict(sequences, lengths, alleles, species)
-        return self._prepare_DataFrame(rows_list)
+        results = self._predict(sequences, lengths, alleles, species)
+        return self._prepare_DataFrame(result[0] for result in results)
     
     def _predict(self, sequences, lengths, alleles, species):
         alleles = list(allele.replace("*", "_") for allele in alleles)
         lengths = ",".join(map(str, lengths))
         seq_file = create_temp_fasta(sequences, self.tempdir)
-        print(seq_file)
+        
         try:
             return list(self._execute(seq_file, lengths, allele)
                 for allele in alleles)
-
         finally:
             #os.remove(seq_file)
             pass
@@ -60,22 +61,43 @@ class LocalNetMHCPanPredictor(MHCPeptidePredictor):
             "-tdir", self.tempdir
         ]
         output = subprocess.check_output(cmd)
-        output = output.split("\n")[19:]
-        ignore = set(("Protein","pos",""))
 
-        def parse_row(row):
-            if not row.startswith("-"):
-                row = re.split("\s*", row.strip())[:9]
-                if len(row) == 9 and row[0] not in ignore:
-                    return row
+        accuracies = []
+        summaries = []
+        rows = []
+        div_count = 0
         
-        return filter(None, map(parse_row, output))
+        for row in output.split("\n"):
+            if row.startswith("#"):
+                continue
+            elif row.startswith("-"):
+                # we found a table divider row
+                div_count += 1
+            elif div_count == 0:
+                # we're before the table
+                accuracies.append(row)
+            elif div_count == 1:
+                # this is the header - ignore
+                pass
+            elif div_count == 2:
+                # we're in the main body of the table
+                rows.append(row)
+            else:
+                # we're after the table
+                summaries.append(row)
+        
+        return (
+            list(row[0:7] for row in
+                csv.reader(rows, skipinitialspace=True)),
+            accuracies, 
+            summaries
+        )
     
     def _prepare_DataFrame(self, rows_list):
         df = rbind(rows_list)
         df.colnames = [
-            "pos", "allele", "peptide", "identity", "pos2", 
-            "core", "1-log50k(aff)", "affinity", "rank"
+            "pos", "allele", "peptide", "identity",
+            "1-log50k(aff)", "affinity", "rank"
         ]
         
         #df = df.drop(["identity", "pos2", "rank"], 1)
